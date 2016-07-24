e8fef987d93893e26a153ce9955842a970b26caa
Ryan Ferraro <jurianknight@gmail.com>
2016-07-24 16:00:42 -0700

    Modified paths and docker tags to better work with dockerhub

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -3,11 +3,11 @@
 echo "removing stale images"
 docker ps -q -a | xargs docker rm
 docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
-docker rmi rferraro/build-essentials/cxx-common:ubuntu-16.04 \
-	rferraro/build-essentials/cxx-gcc-54:ubuntu-16.04 \
-	rferraro/build-essentials/cxx-clang-38:ubuntu-16.04 \
-	rferraro/build-essentials/cxx-gcc-54-conan:0.9.4-custom \
-	rferraro/build-essentials/cxx-clang-38-conan:0.10.1
+docker rmi rferraro/cxx-common \
+	rferraro/cxx-gcc-54 \
+	rferraro/cxx-clang-38 \
+	rferraro/cxx-gcc-54-conan \
+	rferraro/cxx-clang-38-conan
 
 set -e
 
@@ -18,11 +18,11 @@ pushd ..
 
 #docker build --no-cache -t rferraro/build-essentials/cxx-common:ubuntu-16.04 cxx-common
 
-docker build -t rferraro/build-essentials/cxx-common:ubuntu-16.04 build-essentials/cxx-common
-docker build -t rferraro/build-essentials/cxx-gcc-54:ubuntu-16.04 build-essentials/cxx-gcc-54
-docker build -t rferraro/build-essentials/cxx-clang-38:ubuntu-16.04 build-essentials/cxx-clang-38
-docker build -t rferraro/build-essentials/cxx-gcc-54-conan:0.9.4-custom build-essentials/cxx-gcc-54-conan
-docker build -t rferraro/build-essentials/cxx-clang-38-conan:0.10.1 build-essentials/cxx-clang-38-conan
+docker build -t rferraro/cxx-common build-ci/cxx-common
+docker build -t rferraro/cxx-gcc-54 build-ci/cxx-gcc-54
+docker build -t rferraro/cxx-clang-38 build-ci/cxx-clang-38
+docker build -t rferraro/cxx-gcc-54-conan build-ci/cxx-gcc-54-conan
+docker build -t rferraro/cxx-clang-38-conan build-ci/cxx-clang-38-conan
 
 popd
 popd
