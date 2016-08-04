f4acdbccebbcb5bd9e1245094060f93346f79598
Ryan Ferraro <jurianknight@gmail.com>
2016-08-03 19:06:59 -0700

    Reorganized and rebased the Dockerfiles to better suit their needs.

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -3,26 +3,17 @@
 echo "removing stale images"
 docker ps -q -a | xargs docker rm
 docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
-docker rmi rferraro/cxx-common \
-	rferraro/cxx-gcc-54 \
-	rferraro/cxx-clang-38 \
-	rferraro/cxx-gcc-54-conan \
-	rferraro/cxx-clang-38-conan
 
 set -e
 
-ToolsPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#docker build --no-cache -t rferraro/cxx-common:debian-stretch cxx/common/debian/stretch
 
-pushd "$ToolsPath"
-pushd ..
+docker build -t rferraro/cxx-common:debian-stretch cxx/common/debian/stretch
+docker build -t rferraro/cxx-common:alpine-3.4 cxx/common/alpine/3.4
+docker build -t rferraro/cxx-gcc:5.3.0 cxx/gcc/5.3.0
+docker build -t rferraro/cxx-clang:3.8.0 cxx/clang/3.8.0
+docker build -t rferraro/cxx-clang:3.7.1 cxx/clang/3.7.1
+docker build -t rferraro/cxx-travis-ci:gcc-5.3.0 cxx/travis-ci/gcc/5.3.0
+docker build -t rferraro/cxx-travis-ci:clang-3.8.0 cxx/travis-ci/clang/3.8.0
+docker build -t rferraro/cxx-travis-ci:clang-3.7.1 cxx/travis-ci/clang/3.7.1
 
-#docker build --no-cache -t rferraro/build-essentials/cxx-common:ubuntu-16.04 cxx-common
-
-docker build -t rferraro/cxx-common build-ci/cxx-common
-docker build -t rferraro/cxx-gcc-54 build-ci/cxx-gcc-54
-docker build -t rferraro/cxx-clang-38 build-ci/cxx-clang-38
-docker build -t rferraro/cxx-gcc-54-conan build-ci/cxx-gcc-54-conan
-docker build -t rferraro/cxx-clang-38-conan build-ci/cxx-clang-38-conan
-
-popd
-popd
