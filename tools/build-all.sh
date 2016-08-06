ca483f328a6528ec013b68449f4be64825cd330d
Ryan Ferraro <jurianknight@gmail.com>
2016-08-05 22:13:55 -0700

    Corrected images for code coverage

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -1,19 +1,57 @@
 #!/bin/bash
 
-echo "removing stale images"
-docker ps -q -a | xargs docker rm
-docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
-
 set -e
 
-#docker build --no-cache -t rferraro/cxx-common:debian-stretch cxx/common/debian/stretch
+function usage
+{
+    printf "Usage: %s [<options>]\n" $(basename $0)
+    echo ""
+    echo "Available options:"
+    echo "    -s     stop all running containers"
+    echo "    -d     delete all untagged images (implies -s)"
+    echo "    -c     rebuild images without cache"
+}
 
-docker build -t rferraro/cxx-common:debian-stretch cxx/common/debian/stretch
-docker build -t rferraro/cxx-common:alpine-3.4 cxx/common/alpine/3.4
-docker build -t rferraro/cxx-gcc:5.3.0 cxx/gcc/5.3.0
-docker build -t rferraro/cxx-clang:3.8.0 cxx/clang/3.8.0
-docker build -t rferraro/cxx-clang:3.7.1 cxx/clang/3.7.1
-docker build -t rferraro/cxx-travis-ci:gcc-5.3.0 cxx/travis-ci/gcc/5.3.0
-docker build -t rferraro/cxx-travis-ci:clang-3.8.0 cxx/travis-ci/clang/3.8.0
-docker build -t rferraro/cxx-travis-ci:clang-3.7.1 cxx/travis-ci/clang/3.7.1
+opt_stop_all=0
+opt_delete_orphan=0
+build_options=""
+
+while getopts ":sdch?" opt ; do
+    case "$opt" in
+        s)
+            opt_stop_all=1
+            ;;
+        d)
+            opt_stop_all=1
+            opt_delete_orphan=1
+            ;;
+        c)
+            build_options="--no-cache"
+            ;;
+        h|\?)
+            usage
+            exit 0
+            ;;
+    esac
+done
+
+if [ $opt_stop_all -eq 1 ]; then
+	echo "stopping containers"
+	docker ps -q -a | xargs docker rm 2> /dev/null || $true
+fi
+
+if [ $opt_delete_orphan -eq 1 ]; then
+	echo "removing stale images"
+	docker rmi $(docker images | grep "^<none>" | awk '{print $3}') 2> /dev/null || $true
+fi
+
+
+docker build $build_options -t rferraro/cxx-common:debian-stretch cxx/common/debian/stretch
+docker build $build_options -t rferraro/cxx-common:alpine-3.4 cxx/common/alpine/3.4
+docker build $build_options -t rferraro/cxx-gcc:5.3.0 cxx/gcc/5.3.0
+docker build $build_options -t rferraro/cxx-clang:3.8.0 cxx/clang/3.8.0
+docker build $build_options -t rferraro/cxx-clang:3.7.1 cxx/clang/3.7.1
+docker build $build_options -t rferraro/cxx-travis-ci:gcc-5.3.0 cxx/travis-ci/gcc/5.3.0
+docker build $build_options -t rferraro/cxx-travis-ci:clang-3.8.0 cxx/travis-ci/clang/3.8.0
+docker build $build_options -t rferraro/cxx-travis-ci:clang-3.7.1 cxx/travis-ci/clang/3.7.1
 
