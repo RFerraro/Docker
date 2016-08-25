c974bc1f41085bd69773fbd24aa4d63894b2c1c2
Ryan Ferraro <ferraro.ryan.m@gmail.com>
2016-08-24 17:04:10 -0700

    Smaller base images, and additional image layers where they shouldn't add additional storage penalties.

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -7,16 +7,18 @@ function usage
     printf "Usage: %s [<options>]\n" $(basename $0)
     echo ""
     echo "Available options:"
-    echo "    -s     stop all running containers"
-    echo "    -d     delete all untagged images (implies -s)"
-    echo "    -c     rebuild images without cache"
+    echo "    -s        stop all running containers"
+    echo "    -d        delete all untagged images (implies -s)"
+    echo "    -c        rebuild images without cache"
+    echo "    -t <cert> use tls certificate"
 }
 
 opt_stop_all=0
 opt_delete_orphan=0
 build_options=""
+docker_options=""
 
-while getopts ":sdch?" opt ; do
+while getopts ":sdct:h?" opt ; do
     case "$opt" in
         s)
             opt_stop_all=1
@@ -27,6 +29,9 @@ while getopts ":sdch?" opt ; do
             ;;
         c)
             build_options="--no-cache"
+            ;;
+        t)
+            docker_options="--tls=true --tlscacert=\"$OPTARG\""
             ;;
         h|\?)
             usage
@@ -45,13 +50,12 @@ if [ $opt_delete_orphan -eq 1 ]; then
 	docker rmi $(docker images | grep "^<none>" | awk '{print $3}') 2> /dev/null || $true
 fi
 
-
-docker build $build_options -t rferraro/cxx-common:debian-stretch cxx/common/debian/stretch
-docker build $build_options -t rferraro/cxx-common:alpine-3.4 cxx/common/alpine/3.4
-docker build $build_options -t rferraro/cxx-gcc:5.3.0 cxx/gcc/5.3.0
-docker build $build_options -t rferraro/cxx-clang:3.8.0 cxx/clang/3.8.0
-docker build $build_options -t rferraro/cxx-clang:3.7.1 cxx/clang/3.7.1
-docker build $build_options -t rferraro/cxx-travis-ci:gcc-5.3.0 cxx/travis-ci/gcc/5.3.0
-docker build $build_options -t rferraro/cxx-travis-ci:clang-3.8.0 cxx/travis-ci/clang/3.8.0
-docker build $build_options -t rferraro/cxx-travis-ci:clang-3.7.1 cxx/travis-ci/clang/3.7.1
+docker $docker_options build $build_options -t rferraro/cxx-common:debian-stretch ../cxx/common/debian/stretch
+docker $docker_options build $build_options -t rferraro/cxx-common:alpine-3.4 ../cxx/common/alpine/3.4
+docker $docker_options build $build_options -t rferraro/cxx-gcc:5.3.0 ../cxx/gcc/5.3.0
+docker $docker_options build $build_options -t rferraro/cxx-clang:3.8.0 ../cxx/clang/3.8.0
+docker $docker_options build $build_options -t rferraro/cxx-clang:3.7.1 ../cxx/clang/3.7.1
+docker $docker_options build $build_options -t rferraro/cxx-travis-ci:gcc-5.3.0 ../cxx/travis-ci/gcc/5.3.0
+docker $docker_options build $build_options -t rferraro/cxx-travis-ci:clang-3.8.0 ../cxx/travis-ci/clang/3.8.0
+docker $docker_options build $build_options -t rferraro/cxx-travis-ci:clang-3.7.1 ../cxx/travis-ci/clang/3.7.1
 
